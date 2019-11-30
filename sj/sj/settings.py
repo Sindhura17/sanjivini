01275c3896db765ed615a9d7407aabfa542dcedd
Sindhura17 <sindhura.l.bhat@gmail.com>
2019-11-30 12:35:18 +0530

    Update settings.py

--- sj/sj/settings.py
+++ sj/sj/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'sj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'health',
+    'USER': 'root',
+    'PASSWORD': “”,
+    'HOST': “”,
+    'PORT': “”,
+    'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
