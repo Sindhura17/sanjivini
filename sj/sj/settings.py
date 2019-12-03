6e0ddcdd886afe3413639780deaba48bcf7385df
iamspoo <spoorthi246@gmail.com>
2019-12-03 15:57:26 +0530

    frontend

--- sj/sj/settings.py
+++ sj/sj/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,"media files")
+]
