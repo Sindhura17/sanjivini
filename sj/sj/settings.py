c1a4e808fe52e9b702399f6f38293a188d8a7847
iamspoo <spoorthi246@gmail.com>
2020-01-07 23:21:12 +0530

    commit

--- sj/sj/settings.py
+++ sj/sj/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'sj.wsgi.application'
 DATABASES={
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'health',
+        'NAME': 'sanjeevini',
         'USER':'root',
         'PASSWORD':'',
         'HOST':'localhost',
