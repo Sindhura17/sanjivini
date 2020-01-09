291938e170caac6737900dee85aa6020ee99294a
iamspoo <spoorthi246@gmail.com>
2020-01-09 16:50:27 +0530

    latest commit

--- sj/workapp/views.py
+++ sj/workapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import render,redirect
 
 # Create your views here.
+class sign_in(TemplateView):
+    template_name='workapp/sign_in.html'
+    
+    def get(self,request):
+        dform=docForm()
+        nform=ngoForm()
+        return render(request,self.template_name,{'dform':dform,'nform':nform})
+        
+        
