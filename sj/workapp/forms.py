fa9502a7fe02eaea41420a10955e3ee32e1c1739
Sindhura17 <sindhura.l.bhat@gmail.com>
2020-01-10 10:52:12 +0530

    form

--- sj/workapp/forms.py
+++ sj/workapp/forms.py
@@ -3,13 +3,13 @@ from django import forms
 from .models import doc,ngo
 
 class docForm(forms.ModelForm):
-    Username=forms.CharField()
+    '''Username=forms.CharField()
     Password=forms.CharField(widget=forms.PasswordInput())
     Email=forms.EmailField()
     Name=forms.CharField()
     Phone=forms.CharField()
     City=forms.CharField()
-    Specialization=forms.CharField()
+    Specialization=forms.CharField()'''
     
     class Meta:
         model=doc
@@ -17,13 +17,13 @@ class docForm(forms.ModelForm):
 
 
 class ngoForm(forms.ModelForm):
-    Username=forms.CharField()
+    '''Username=forms.CharField()
     Password=forms.CharField(widget=forms.PasswordInput())
     Email=forms.EmailField()
     Name=forms.CharField()
     Phone=forms.CharField()
     Website=forms.URLField()
-    Description=forms.CharField(widget=forms.Textarea(attrs={"rows":5,"cols":20}))
+    Description=forms.CharField(widget=forms.Textarea(attrs={"rows":5,"cols":20}))'''
     class Meta:
         model=ngo
         fields=('username','password','email','name','ph','link','text',)
