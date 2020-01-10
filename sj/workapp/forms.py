4f20ad976ca7742d5f0ce455f0fafeb29891469d
iamspoo <spoorthi246@gmail.com>
2020-01-10 12:48:48 +0530

    commit

--- sj/workapp/forms.py
+++ sj/workapp/forms.py
@@ -3,20 +3,18 @@ from django import forms
 from .models import doc,ngo
 
 class docForm(forms.ModelForm):
-    '''Username=forms.CharField()
-    Password=forms.CharField(widget=forms.PasswordInput())
-    Email=forms.EmailField()
-    Name=forms.CharField()
-    Phone=forms.CharField()
-    City=forms.CharField()
-    Specialization=forms.CharField()'''
     
     class Meta:
         model=doc
         fields=('username','password','email','name','phone','city','special',)
         widgets={
-            'name':forms.TextInput(
-            attrs={'class':"form-control"})
+            'name':forms.TextInput(attrs={'class':"form-control"}),
+            'username':forms.TextInput(attrs={'class':"form-control"}),
+            'email':forms.TextInput(attrs={'class':"form-control"}),
+            'password':forms.TextInput(attrs={'class':"form-control"}),
+            'phone':forms.TextInput(attrs={'class':"form-control"}),
+            'city':forms.TextInput(attrs={'class':"form-control"}),
+            'special':forms.TextInput(attrs={'class':"form-control"}),
             }
 
 
@@ -31,4 +29,13 @@ class ngoForm(forms.ModelForm):
     class Meta:
         model=ngo
         fields=('username','password','email','name','ph','link','text',)
+        widgets={
+            'name':forms.TextInput(attrs={'class':"form-control"}),
+            'username':forms.TextInput(attrs={'class':"form-control"}),
+            'email':forms.TextInput(attrs={'class':"form-control"}),
+            'password':forms.TextInput(attrs={'class':"form-control"}),
+            'ph':forms.TextInput(attrs={'class':"form-control"}),
+            'link':forms.TextInput(attrs={'class':"form-control"}),
+            'text':forms.Textarea(attrs={"rows":5,"cols":20}),
+            }
 
