28e270615605af8487e35bba941d21eeee023c13
iamspoo <spoorthi246@gmail.com>
2020-01-15 18:15:59 +0530

    signin view commit

--- sj/workapp/urls.py
+++ sj/workapp/urls.py
@@ -7,5 +7,7 @@ path('sign_up/',views.sign_up,name='sign_up'),
 path('docsign/',views.docsign,name='docsign'),
 path('ngosign/',views.ngosign,name='ngosign'),
 path('register/',views.register,name='register'),
-path('nregister/',views.register,name='nregister'),
+path('nregister/',views.nregister,name='nregister'),
+path('ngosign_in/',views.ngosign_in,name='ngosign_in'),
+path('docsign_in/',views.docsign_in,name='docsign_in'),
 ]
