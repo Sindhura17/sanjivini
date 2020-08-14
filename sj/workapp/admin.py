d658ecf3fb01e8fb4b45d2c11c43bee4d47107bc
Sindhura17 <sindhura.l.bhat@gmail.com>
2020-08-14 22:39:16 +0530

    home page footer

--- sj/workapp/admin.py
+++ sj/workapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import doc,ngo,event,patient,medication,doregis
 # Register your models here.
+admin.site.register(doc)
+admin.site.register(ngo)
+admin.site.register(event)
+admin.site.register(patient)
+admin.site.register(medication)
+admin.site.register(doregis)
